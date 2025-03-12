692d31bc01322e98f844c3a729649f3a3a61ac6e
Mehedi0014 <mehedi0014@gmail.com>
2025-03-13 01:09:46 +0600

    complete single page and backup database

--- wp-content/themes/tutorstarter-child/assets/js/main.js
+++ wp-content/themes/tutorstarter-child/assets/js/main.js
@@ -1,25 +1,11 @@
 jQuery(document).ready(function($) {
-   
-
-
-    var swiper = new Swiper(".annoorSwiper", {
-        loop: true,
-        speed: 500,
-        autoplay: true,
-        navigation: {
-          nextEl: ".annoor-prev",
-          prevEl: ".annoor-next",
-        },
-      });
-      
-
-
-
-
-
-
-
-
-
-
+  var swiper = new Swiper(".annoorSwiper", {
+    loop: true,
+    speed: 500,
+    autoplay: true,
+    navigation: {
+      nextEl: ".annoor-prev",
+      prevEl: ".annoor-next",
+    },
+  });
 });
